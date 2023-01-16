5eafb12b00113ef7d957ab5b99672eb49ba89492
Bhargaviuduthala <122012380+Bhargaviuduthala@users.noreply.github.com>
2023-01-16 15:58:07 +0530

    Update python_notebook(jenkins).py

--- python_notebook(jenkins).py
+++ python_notebook(jenkins).py
@@ -1,6 +1,5 @@
 l = [2,4,6,8,10]
 square = [i**2 for i in l]
-print('square of the numbers are:', square)
+print('square of the numbers are:')
 cube = [i**3 for i in l]
-print('cube of the numbers are:',cube)
-
+print('cube of the numbers are:')
